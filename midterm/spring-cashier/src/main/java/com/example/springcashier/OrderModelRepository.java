d867ef2df5ba283aff4736846f954087acb83ba9
Minh Quan Le <90435027+QuanLew@users.noreply.github.com>
2023-05-10 19:06:22 -0700

    Delete midterm directory

--- midterm/spring-cashier/src/main/java/com/example/springcashier/OrderModelRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springcashier;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderModelRepository extends CrudRepository<Order, Integer> {
-}
--- midterm/spring-cashier/src/main/java/com/example/springcashier/OrderQueryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springcashier;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderQueryRepository extends JpaRepository<Order, Integer> {
-}
